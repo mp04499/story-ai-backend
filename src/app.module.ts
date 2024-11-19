2d0e7e55633cc3987037ae3c58942ee477f3dc87
mp04499 <michael.powell13@gmail.com>
2024-11-18 19:36:10 -0800

    Clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { StoryController } from './controllers';
+import { StoryService } from './services/story.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath: '.env.local' })],
-  controllers: [AppController, StoryController],
-  providers: [AppService],
+  controllers: [StoryController],
+  providers: [StoryService],
 })
 export class AppModule {}
